ccb38dcd07896183251e463065f9130175df1513
Katarina Caetano <katarinacaetano09@gmail.com>
2025-10-02 14:56:57 -0300

    build: verificar build e salvar configurações atuais

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: false },
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/supabase'],
   css: [
     '~/assets/css/toast.css'
   ]
